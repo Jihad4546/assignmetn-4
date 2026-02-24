0cefb6f5ab51381cdf4b1718a27c09452345258c
jihad <jihadhosen4546@gmail.com>
2026-02-24 15:50:35 +0600

    cominit

--- script/script.js
+++ script/script.js
@@ -37,9 +37,15 @@ document.querySelectorAll(".interviewBtn").forEach((btn) => {
     if (prevStatus === "rejected") rejectedCount--;
     if (prevStatus !== "interview") interviewCount++;
     card.setAttribute("data-status", "interview");
-    interviewJobsContainer.appendChild(card);
+    const coloneCard = card.cloneNode(true);
+    coloneCard.setAttribute('data-from','interview')
+    interviewJobsContainer.appendChild(coloneCard);
     const interviewHidden = document.getElementById("interviewHidden");
     interviewHidden.classList.add("hidden");
+    const approveBtn = this.closest(".job-card").querySelector(".interviewAprove");
+    approveBtn.classList.remove("hidden");
+       const approveBtn1 =this.closest(".job-card").querySelector(".requiredApply");
+    approveBtn1.classList.add("hidden");
     updateCounts();
   });
 });
@@ -47,13 +53,19 @@ document.querySelectorAll(".interviewBtn").forEach((btn) => {
 document.querySelectorAll(".rejectBtn").forEach((btn) => {
   btn.addEventListener("click", function () {
     const card = this.closest(".job-card");
+    console.log(card);
     const prevStatus = card.getAttribute("data-status");
+    console.log(prevStatus)
     if (prevStatus === "interview") interviewCount--;
     if (prevStatus !== "rejected") rejectedCount++;
-    card.setAttribute("data-status", "rejected");
+    card.setAttribute("data-from", "rejected");
     rejectedJobsContainer.appendChild(card);
     const rejectedJobs = document.getElementById("rejectedHidden");
     rejectedJobs.classList.add("hidden");
+    const approveBtn =this.closest(".job-card").querySelector(".requiredApply");
+    approveBtn.classList.remove("hidden");
+    const approveBtn1 =this.closest(".job-card").querySelector(".interviewAprove");
+    approveBtn1.classList.add("hidden");
     updateCounts();
   });
 });
@@ -62,33 +74,36 @@ document.getElementById("allTab").addEventListener("click", () => {
   allJobsContainer.classList.remove("hidden");
   interviewJobsContainer.classList.add("hidden");
   rejectedJobsContainer.classList.add("hidden");
-  const allTab = document.getElementById('allTab');
-  allTab.classList.add('btn-primary')
-  const interviewTab = document.getElementById('interviewTab');
-  interviewTab.classList.remove('btn-primary')
-  const rejectedTab = document.getElementById('rejectedTab');
-  rejectedTab.classList.remove('btn-primary')
-  
+  const allTab = document.getElementById("allTab");
+  allTab.classList.add("btn-primary");
+  const interviewTab = document.getElementById("interviewTab");
+  interviewTab.classList.remove("btn-primary");
+  const rejectedTab = document.getElementById("rejectedTab");
+  rejectedTab.classList.remove("btn-primary");
 });
+
+
+
 document.getElementById("interviewTab").addEventListener("click", () => {
-  const allTab = document.getElementById('allTab');
-  allTab.classList.remove('btn-primary')
-  const rejectedTab = document.getElementById('rejectedTab');
-  rejectedTab.classList.remove('btn-primary')
-  const interviewTab = document.getElementById('interviewTab');
-  interviewTab.classList.add('btn-primary')
+  const allTab = document.getElementById("allTab");
+  allTab.classList.remove("btn-primary");
+  const rejectedTab = document.getElementById("rejectedTab");
+  rejectedTab.classList.remove("btn-primary");
+  const interviewTab = document.getElementById("interviewTab");
+  interviewTab.classList.add("btn-primary");
   allJobsContainer.classList.add("hidden");
   interviewJobsContainer.classList.remove("hidden");
   rejectedJobsContainer.classList.add("hidden");
 });
+
 document.getElementById("rejectedTab").addEventListener("click", () => {
   allJobsContainer.classList.add("hidden");
   interviewJobsContainer.classList.add("hidden");
   rejectedJobsContainer.classList.remove("hidden");
-  const allTab = document.getElementById('allTab');
-  allTab.classList.remove('btn-primary')
-  const rejectedTab = document.getElementById('rejectedTab');
-  rejectedTab.classList.add('btn-primary')
-  const interviewTab = document.getElementById('interviewTab');
-  interviewTab.classList.remove('btn-primary')
+  const allTab = document.getElementById("allTab");
+  allTab.classList.remove("btn-primary");
+  const rejectedTab = document.getElementById("rejectedTab");
+  rejectedTab.classList.add("btn-primary");
+  const interviewTab = document.getElementById("interviewTab");
+  interviewTab.classList.remove("btn-primary");
 });
